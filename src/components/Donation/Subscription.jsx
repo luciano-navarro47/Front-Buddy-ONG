eaf3bdf4ffe5392caf22487684e732ecaab9ffec
Coderman47 <askinglucho@gmail.com>
2025-06-19 18:59:16 -0300

    Change to uppercase the button title in Subscription comp.

--- src/components/Donation/Subscription.jsx
+++ src/components/Donation/Subscription.jsx
@@ -148,7 +148,7 @@ export default function Subscription() {
             isLoading={isLoading}
             isDisabled={isLoading}
           >
-            Suscribite
+            SUSCRIBITE
           </Button>
         </Box>
       </VStack>
