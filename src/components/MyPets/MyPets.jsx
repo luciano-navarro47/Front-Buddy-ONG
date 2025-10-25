6efd1f8a682a515f03396fc47a99a434f581de7b
Coderman47 <askinglucho@gmail.com>
2025-10-25 16:04:48 -0300

    upd: comment the MyPets components

--- src/components/MyPets/MyPets.jsx
+++ src/components/MyPets/MyPets.jsx
@@ -1,60 +1,57 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import s from "./MyPets.module.css";
-import { Heading, Container, Text, Center, Box } from "@chakra-ui/react";
-import Card from "../PetsPosted/PetPostedCard";
-import { getPetsByUser } from "redux/Actions/petActions";
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import s from "./MyPets.module.css";
+// import { Heading, Container, Text, Center, Box } from "@chakra-ui/react";
+// import Card from "../PetsPosted/PetPostedCard";
+// import { getPetsByUser } from "redux/Actions/petActions";
 
-export default function MyPets({ user, setUser }) {
-  console.log("Rendering MyPets")
-  const dispatch = useDispatch();
-  const userPets = useSelector((state) => state.pets.userPets);
-  console.log("U.P: ", userPets)
+// export default function MyPets({ user, setUser }) {
+//   const dispatch = useDispatch();
+//   const userPets = useSelector((state) => state.pets.userPets);
 
-  useEffect(() => {
-    if (userPets === undefined || userPets.length === 0) {
-      dispatch(getPetsByUser(user.id));
-    }
-  }, [dispatch, userPets]);
+//   useEffect(() => {
+//     if (userPets === undefined || userPets.length === 0) {
+//       dispatch(getPetsByUser(user.id));
+//     }
+//   }, [dispatch, userPets, user.id]);
 
-  return (
-    <>
-      <Box bg={"brand.green.200"} mb="3rem">
-        <Heading as="h1" fontSize="2.5rem" pt="3rem" color="gray.800">
-          {" "}
-          Mis mascotas
-        </Heading>
-        {/* DETAIL */}
-        <Container>
-          <Text
-            fontFamily={"body"}
-            fontWeight={"300"}
-            /* noOfLines={[4, 4, 3]} */
-            px="1rem"
-            pt={["0.5rem", "0.5rem", "1rem"]}
-            pb="2rem"
-            my="0rem"
-            fontSize={{ base: "20px", md: "20px", lg: "22px" }}
-            color="gray.500"
-          >
-            Aquí están todas tus publicaciones, podrás editar su información,
-            tanto como para borrarlas
-          </Text>
-        </Container>
-      </Box>
-      <Center>
-        <div className={s.divContainer}>
-          {userPets && userPets[0] ? (
-            userPets.map((pet) => <Card data={pet} value={"update"} />)
-          ) : (
-            <Box display="flex" justifyContent={"center"} width={"100%"}>
-              <Text fontSize="2rem" marginBottom={"2rem"}>
-                No tienes mascotas publicadas
-              </Text>
-            </Box>
-          )}
-        </div>
-      </Center>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Box bg={"brand.green.200"} mb="3rem">
+//         <Heading as="h1" fontSize="2.5rem" pt="3rem" color="gray.800">
+//           {" "}
+//           Mis mascotas
+//         </Heading>
+//         {/* DETAIL */}
+//         <Container>
+//           <Text
+//             fontFamily={"body"}
+//             fontWeight={"300"}
+//             px="1rem"
+//             pt={["0.5rem", "0.5rem", "1rem"]}
+//             pb="2rem"
+//             my="0rem"
+//             fontSize={{ base: "20px", md: "20px", lg: "22px" }}
+//             color="gray.500"
+//           >
+//             Aquí están todas tus publicaciones, podrás editar su información,
+//             tanto como para borrarlas
+//           </Text>
+//         </Container>
+//       </Box>
+//       <Center>
+//         <div className={s.divContainer}>
+//           {userPets && userPets[0] ? (
+//             userPets.map((pet) => <Card data={pet} value={"update"} />)
+//           ) : (
+//             <Box display="flex" justifyContent={"center"} width={"100%"}>
+//               <Text fontSize="2rem" marginBottom={"2rem"}>
+//                 No tienes mascotas publicadas
+//               </Text>
+//             </Box>
+//           )}
+//         </div>
+//       </Center>
+//     </>
+//   );
+// }
