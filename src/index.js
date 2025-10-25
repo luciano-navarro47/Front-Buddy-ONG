2156453b5b0b83e2cf757afac3cf3ff93a4ee482
Coderman47 <askinglucho@gmail.com>
2025-10-25 18:52:58 -0300

    rename: use lowercase in the name of the folders inside commons

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/store";
+import { store } from "redux/store/Index";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
-import App from "./app";
+import App from "./App"
 import reportWebVitals from "./reportWebVitals";
-import { theme } from "./components/commons/theme";
+import { theme } from "components/commons/theme";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
