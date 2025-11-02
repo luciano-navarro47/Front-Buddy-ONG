d8a422eed9f3746d04c7ac9966be4180dcda60c0
Coderman47 <askinglucho@gmail.com>
2025-11-01 21:15:58 -0300

    fix/setLoadingPet in false when the usePetform hook fill the petdata

--- src/utils/hooks/pet/usePetForm.js
+++ src/utils/hooks/pet/usePetForm.js
@@ -6,7 +6,6 @@ export function usePetForm(paramsId, initialValues, setInput, isUpdating) {
   const dispatch = useDispatch();
   const petData = useSelector((s) => s.pets.petDetails);
 
-
   useEffect(() => {
     if (paramsId) dispatch(getPetDetails(paramsId));
   }, [dispatch, paramsId]);
@@ -15,6 +14,7 @@ export function usePetForm(paramsId, initialValues, setInput, isUpdating) {
     setInput(
       isUpdating === true ? { ...initialValues, ...petData } : initialValues
     );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [petData, paramsId, isUpdating, setInput]);
 
   return petData;
